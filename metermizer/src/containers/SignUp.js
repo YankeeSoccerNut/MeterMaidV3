7d965848bd9d3b18ec6eab849731af0192709b28
Scott Anderson <scott@v1apps.com>
2018-09-14 00:34:30 -0400

    wired up mutation to signup form...initial tests work

--- metermizer/src/containers/SignUp.js
+++ metermizer/src/containers/SignUp.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
 
 import PropTypes from 'prop-types';
 import Avatar from '@material-ui/core/Avatar';
@@ -14,6 +15,17 @@ import Typography from '@material-ui/core/Typography';
 import withStyles from '@material-ui/core/styles/withStyles';
 import styles from '../styles/SignUp';
 
+const SIGNUP_USER = gql`
+  mutation SignUp($email: String!, $smsphone: String!, $password: String!) {
+    createUser(
+      input: { user: { email: $email, phone: $smsphone, password: $password } }
+    ) {
+      user {
+        id
+      }
+    }
+  }
+`;
 class SignUp extends PureComponent {
   constructor(props) {
     super(props);
@@ -32,6 +44,7 @@ class SignUp extends PureComponent {
   }
 
   render() {
+    const { email, smsphone, password } = this.state;
     return (
       <React.Fragment>
         <CssBaseline />
@@ -41,80 +54,84 @@ class SignUp extends PureComponent {
               <LockIcon />
             </Avatar>
             <Typography variant="headline">New Account Sign Up</Typography>
-            <Mutation>
-              <form className={this.props.classes.form}>
-                <FormControl margin="normal" required fullWidth>
-                  <InputLabel htmlFor="companyname">Company Name</InputLabel>
-                  <Input
-                    name="companyname"
-                    type="text"
-                    id="companyname"
-                    autoComplete="company-name"
-                    onChange={e => this.handleChange(e)}
-                  />
-                </FormControl>
-                <FormControl margin="normal" required fullWidth>
-                  <InputLabel htmlFor="firstname">First Name</InputLabel>
-                  <Input
-                    name="firstname"
-                    type="text"
-                    id="firstname"
-                    autoComplete="first-name"
-                    onChange={e => this.handleChange(e)}
-                  />
-                </FormControl>
-                <FormControl margin="normal" required fullWidth>
-                  <InputLabel htmlFor="lastname">Last Name</InputLabel>
-                  <Input
-                    name="lastname"
-                    type="text"
-                    id="lastname"
-                    autoComplete="last-name"
-                    onChange={e => this.handleChange(e)}
-                  />
-                </FormControl>
-                <FormControl margin="normal" required fullWidth>
-                  <InputLabel htmlFor="phone">Phone</InputLabel>
-                  <Input
-                    name="smsphone"
-                    type="phone"
-                    id="smsphone"
-                    autoComplete="phone"
-                    onChange={e => this.handleChange(e)}
-                  />
-                </FormControl>
-                <FormControl margin="normal" required fullWidth>
-                  <InputLabel htmlFor="email">Email Address</InputLabel>
-                  <Input
-                    id="email"
-                    name="email"
-                    autoComplete="email"
-                    autoFocus
-                    onChange={e => this.handleChange(e)}
-                  />
-                </FormControl>
-                <FormControl margin="normal" required fullWidth>
-                  <InputLabel htmlFor="password">Password</InputLabel>
-                  <Input
-                    name="password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                    onChange={e => this.handleChange(e)}
-                  />
-                </FormControl>
-                <Button
-                  type="submit"
-                  onClick={e => this.submitForm(e)}
-                  fullWidth
-                  variant="raised"
-                  color="primary"
-                  className={this.props.classes.submit}
-                >
-                  Sign Up
-                </Button>
-              </form>
-            </Mutation>
+            <form className={this.props.classes.form}>
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="companyname">Company Name</InputLabel>
+                <Input
+                  name="companyname"
+                  type="text"
+                  id="companyname"
+                  autoComplete="company-name"
+                  onChange={e => this.handleChange(e)}
+                />
+              </FormControl>
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="firstname">First Name</InputLabel>
+                <Input
+                  name="firstname"
+                  type="text"
+                  id="firstname"
+                  autoComplete="first-name"
+                  onChange={e => this.handleChange(e)}
+                />
+              </FormControl>
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="lastname">Last Name</InputLabel>
+                <Input
+                  name="lastname"
+                  type="text"
+                  id="lastname"
+                  autoComplete="last-name"
+                  onChange={e => this.handleChange(e)}
+                />
+              </FormControl>
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="phone">Phone</InputLabel>
+                <Input
+                  name="smsphone"
+                  type="phone"
+                  id="smsphone"
+                  autoComplete="phone"
+                  onChange={e => this.handleChange(e)}
+                />
+              </FormControl>
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="email">Email Address</InputLabel>
+                <Input
+                  id="email"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  onChange={e => this.handleChange(e)}
+                />
+              </FormControl>
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="password">Password</InputLabel>
+                <Input
+                  name="password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  onChange={e => this.handleChange(e)}
+                />
+              </FormControl>
+              <Mutation
+                mutation={SIGNUP_USER}
+                variables={{ email, smsphone, password }}
+              >
+                {postSignUp => (
+                  <Button
+                    onClick={postSignUp}
+                    fullWidth
+                    variant="raised"
+                    color="primary"
+                    className={this.props.classes.submit}
+                  >
+                    Submit
+                  </Button>
+                )}
+              </Mutation>
+            </form>
           </Paper>
         </main>
       </React.Fragment>
