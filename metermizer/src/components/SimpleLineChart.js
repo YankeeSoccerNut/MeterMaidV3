18df8442011e0e79d5b7d7b4f8a18f63726bf388
Scott Anderson <scott@v1apps.com>
2018-09-22 09:22:36 -0400

    simple line chart working with readings

--- metermizer/src/components/SimpleLineChart.js
+++ metermizer/src/components/SimpleLineChart.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import ResponsiveContainer from 'recharts/lib/component/ResponsiveContainer';
 import LineChart from 'recharts/lib/chart/LineChart';
 import Line from 'recharts/lib/cartesian/Line';
@@ -8,36 +8,33 @@ import CartesianGrid from 'recharts/lib/cartesian/CartesianGrid';
 import Tooltip from 'recharts/lib/component/Tooltip';
 import Legend from 'recharts/lib/component/Legend';
 
-const data = [
-  { name: 'Mon', Visits: 2200, Orders: 3400 },
-  { name: 'Tue', Visits: 1280, Orders: 2398 },
-  { name: 'Wed', Visits: 5000, Orders: 4300 },
-  { name: 'Thu', Visits: 4780, Orders: 2908 },
-  { name: 'Fri', Visits: 5890, Orders: 4800 },
-  { name: 'Sat', Visits: 4390, Orders: 3800 },
-  { name: 'Sun', Visits: 4490, Orders: 4300 }
-];
+class SimpleLineChart extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
 
-function SimpleLineChart(props) {
-  console.log('simple line chart props', props);
-  return (
-    // 99% per https://github.com/recharts/recharts/issues/172
-    <ResponsiveContainer width="99%" height={320}>
-      <LineChart data={data}>
-        <XAxis dataKey="name" />
-        <YAxis />
-        <CartesianGrid vertical={false} strokeDasharray="3 3" />
-        <Tooltip />
-        <Legend />
-        <Line
-          type="monotone"
-          dataKey="Readings"
-          stroke="#8884d8"
-          activeDot={{ r: 8 }}
-        />
-      </LineChart>
-    </ResponsiveContainer>
-  );
+  render() {
+    const { classes, readings } = this.props;
+
+    return (
+      // 99% per https://github.com/recharts/recharts/issues/172
+      <ResponsiveContainer width="99%" height={320}>
+        <LineChart data={readings}>
+          <XAxis dataKey="thermCreatedAt" />
+          <YAxis />
+          <CartesianGrid vertical={false} strokeDasharray="3 3" />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="dispTemp"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    );
+  }
 }
 
 export default SimpleLineChart;
